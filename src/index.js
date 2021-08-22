b56f4f51ae849a93748f0fcb3da1f4f640cb8fd3
Daniyal Tariq <daniyaltariq@Daniyals-MacBook-Pro.local>
2021-08-22 23:20:05 +0500

    socket io installed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
-const port = process.env.port || 3000
+const server = http.createServer(app)
+const io = socketio(server)
 
+const port = process.env.port || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', (io) => {
+    console.log('web socket connected')
+    io.on('disconnect', () => {
+        console.log('web socket disconnected')
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is up and running on ${port}`)
 })
