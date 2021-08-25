d2723ad863468e7d9362d241afe8695d4ae004c4
Daniyal Tariq <daniyaltariq@Daniyals-MacBook-Pro.local>
2021-08-26 04:37:21 +0500

    socket integrated with browser location sharing feature

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,10 +13,30 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', (io) => {
+io.on('connection', (socket) => {
     console.log('web socket connected')
-    io.on('disconnect', () => {
-        console.log('web socket disconnected')
+    socket.emit('message', 'Welcome!')
+
+    socket.broadcast.emit('message', 'A user has joined')
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed')
+        }
+
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
