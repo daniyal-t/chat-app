a8c7bba331818b1ec5722667f79cf35ffd8c9670
Daniyal Tariq <daniyal.t@protonmail.ch>
2021-09-02 22:34:04 +0500

    timestamp added with chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,74 @@
-const socket = io()
+const socket = io();
 
-socket.on('message', (message) => {
-    console.log(message)
-})
+// ELements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
-    e.preventDefault()
+// Template
 
-    const message = e.target.elements.message.value
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
 
-    socket.emit('sendMessage', message, (error) => {
-       if (error) {
-           return console.log(error)
-       }
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    createdAt: moment(message.createdAt).format("h:mm a"),
+    message: message.text,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-       console.log('Message delivered!')
-    })
-})
+socket.on("locationMessage", (message) => {
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-document.querySelector('#send-location').addEventListener('click', () => {
-    if (!navigator.geolocation) {
-        return alert('Geolocation is not supported by your browser')
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled", "disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
     }
 
-    navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit('sendLocation', {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        }, () => {
-            console.log('Location Shared')
-        })
-    })
-})
+    console.log("Message delivered!");
+  });
+});
+
+$sendLocationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        $sendLocationButton.removeAttribute("disabled", "disabled");
+        console.log("Location Shared");
+      }
+    );
+  });
+});
