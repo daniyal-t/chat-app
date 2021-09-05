7e3aa68d5403290ee0a036b34c83819bd6400b13
Daniyal Tariq <daniyal.t@protonmail.ch>
2021-09-05 23:29:37 +0500

    users side bar add, scroll with messages add, new user names printing

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,12 +1,14 @@
-const generateMessage = (text) => {
+const generateMessage = (username, text) => {
   return {
+    username,
     text,
     createdAt: new Date().getTime(),
   };
 };
 
-const generateLocationMessages = (url) => {
+const generateLocationMessages = (username, url) => {
   return {
+    username,
     url,
     createdAt: new Date().getTime(),
   };
